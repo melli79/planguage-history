162a80777cca9cd67bef0d223b90bc9c44f29944
Melchior <melchiorG@gMail.com>
2025-11-27 00:54:31 +0100

    bump gradle version.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -28,6 +28,7 @@ kotlin {
         iosTarget.binaries.framework {
             baseName = "ComposeApp"
             isStatic = true
+            freeCompilerArgs += "-Xbinary=bundleId=org.grutzmann.mda"
         }
     }
     
